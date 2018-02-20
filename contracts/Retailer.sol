b80fa8bad6a060af609e0347afca16043cf66e73
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2018-02-20 08:52:18 +0100

    Retailer list as ERC721

--- contracts/Retailer.sol
+++ contracts/Retailer.sol
@@ -1,24 +1,72 @@
 pragma solidity 0.4.20;
 
-import {Managed} from "./libs/ownership/Managed.sol";
+import {ERC721Token} from "./libs/token/ERC721/ERC721Token.sol";
+import {Pausable} from "./libs/lifecycle/Pausable.sol";
 
 
-contract Retailer is Managed {
-  string public name;
-  string public description;
-  string public url;
-  bytes32 public pubKey;
+contract Retailers is ERC721Token, Pausable {
   
-  function Retailer(
-    string _name, 
-    string _description, 
-    string _url,
-    bytes32 _pubKey
-    ) public 
-  {
-    name = _name;
-    description = _description;
-    url = _url;
-    pubKey = _pubKey;
+  struct Retailer {
+    string name;
+    string description;
+    string url;
+    bytes32 pubKey;
   }
+
+  Retailer[] public retailers;
+
+  function addRetailer(string _name, string _description, string _url, bytes32 _pubKey) public returns (uint retailerId) {
+    bytes memory name = bytes(_name);
+    require(bytes(name).length > 0);
+    require(_pubKey[0] != 0);
+
+    Retailer memory _retailer = Retailer({
+      name: _name,
+      description: _description,
+      url: _url,
+      pubKey: _pubKey
+    });
+    uint256 opId = retailers.push(_retailer) - 1;
+
+    _mint(msg.sender, opId);
+
+    return opId;
+  }
+
+  function retailerById(uint _retailerId) internal onlyOwnerOf(_retailerId) returns(Retailer) {
+    return retailers[_retailerId];
+  }
+  
+  function nameById(uint _retailerId) public returns(string) {
+    return retailerById(_retailerId).name;
+  }
+
+  function setNameById(uint _retailerId, string _name) public onlyOwnerOf(_retailerId) {
+    retailerById(_retailerId).name = _name;
+  }
+
+  function descriptionById(uint _retailerId) public returns(string) {
+    return retailerById(_retailerId).description;
+  }
+
+  function setDescriptionById(uint _retailerId, string _description) public onlyOwnerOf(_retailerId) {
+    retailerById(_retailerId).description = _description;
+  }
+
+  function urlById(uint _retailerId) public onlyOwnerOf(_retailerId) returns(string) {
+    return retailerById(_retailerId).url;
+  }
+
+  function setUrlById(uint _retailerId, string _url) public onlyOwnerOf(_retailerId) {
+    retailerById(_retailerId).url = _url;
+  }
+
+  function pubKeyById(uint _retailerId) public onlyOwnerOf(_retailerId) returns(bytes32) {
+    return retailerById(_retailerId).pubKey;
+  }
+
+  function setPubKey(uint _retailerId, bytes32 _pubKey) public onlyOwnerOf(_retailerId) {
+    retailerById(_retailerId).pubKey = _pubKey;
+  }
+
 }
