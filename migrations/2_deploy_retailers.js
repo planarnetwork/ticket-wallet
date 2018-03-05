a6055b7d5ca09620b2ea8aacb5df6658d2ddadde
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2018-03-05 23:34:09 +0100

    Remove imports

--- migrations/2_deploy_retailers.js
+++ migrations/2_deploy_retailers.js
@@ -1,6 +1,4 @@
 var Retailers = artifacts.require("Retailers");
-var ECTools = artifacts.require("ECTools");
-var TicketWallet = artifacts.require("TicketWallet");
 
 module.exports = (deployer) => {
 	deployer.deploy(Retailers)
