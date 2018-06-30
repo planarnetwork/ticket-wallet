9fd0f73cfb85e9495f5bbbbd86b3931971107fc5
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2018-06-30 22:46:53 +0100

    Testnet

--- truffle.js
+++ truffle.js
@@ -1,15 +1,26 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "###";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    test: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/VfWYr7OjdspjLol0LX6t");
+      },
+      network_id: 3,
+      gas: 4700000
     }
   },
   mocha: {
-    reporter: 'eth-gas-reporter',
+    reporter: "eth-gas-reporter",
     reporterOptions : {
-      currency: 'USD',
+      currency: "USD",
       gasPrice: 21
     }
   },
